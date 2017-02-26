851663b636d93a8aae56e1760fdb5640a5cd56b9
blukai <init1@protonmail.com>
2017-02-26 19:06:28 +0200

    Test deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+REPO=`git config remote.origin.url`
+# Use token for pushing below
+TOKENED=${REPO/github.com/$GH_TOKEN@github.com}
+
+git config --global user.name "Travis CI"
+git config --global user.email "travis@travis-ci.org"
+
+git clone --branch gh-pages $REPO deploy
+
+cp index.html 404.html
+mv -v build index.html 404.html deploy
+
+cd deploy
+
+git remote add upstream $TOKENED
+
+git add --all .
+
+if [ -n "$(git status --porcelain)" ];
+then
+  git commit -m "Build $TRAVIS_COMMIT"
+  git push --quiet upstream HEAD:gh-pages
+else
+  exit 0
+fi
